0bf7c3bbca772898c9f413fa42872e5951a2be83
jorabada <jorge.rabadan@nokia.com>
2025-01-31 09:53:47 -0800

    adding more clients

---
+++ configs-linux/s4.sh
@@ -0,0 +1,6 @@
+ip link add link eth1 name eth1.1 type vlan id 1
+ip addr add 10.0.3.1/24 dev eth1.1
+ip link set eth1.1 up
+ip route add 10.0.0.0/24 via 10.0.3.254
+ip route add 10.0.1.0/24 via 10.0.3.254
+ip route add 10.0.2.0/24 via 10.0.3.254
